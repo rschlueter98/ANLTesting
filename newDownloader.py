8ee3c6a04f941823226d948538822bd45f9ae208
rschlueter98 <rschlueter98@gmail.com>
2018-02-17 17:32:31 -0500

    redo

--- newDownloader.py
+++ newDownloader.py
@@ -1,40 +1,46 @@
-#######################################################################################################################
-#Header
-#This was made by Ryan Schlueter rschlueter98@gmail.com, (314) 603-4504, BSEE '18 on Sep. 18th 2017
-#I'm not a coder, so don't hate if this doesn't follow PEP-8 or whatever other standards
-#
-#This program downloads images from a list of m3u8 streams. It doesn't save them or run yolo or anything
-#Run it with: python image_downloader.py <input_filename> <type> <inputValue>
-#input_filename is the filename of m3u8 streams you want to download from
-#type is either "num" or "time" based of if you want to download for x minutes, or download x images
-#inputValue is the above mentioned x. Like 5000 images, or 60 minutes
-#
-#No output files, FPS is display at the end
-#######################################################################################################################
-
 import time
 import cv2
 
 
 
 if __name__ == '__main__':
-  downloadCounter=0
+  import cv2
 
-  url="http://video3.earthcam.com/fecnetwork/10347.flv/playlist.m3u8"
-  stream = cv2.VideoCapture(url)
-  print("Stream opened")
-  time.sleep(0.5)
-  for x in range (0,10):
-    frame = stream.read()[1]
-    print(frame)
-    filename = ("z_" + str(x) + ".jpg")
-    fullpath = ("home/rschluet/imageDownloads/" + filename)
-    cv2.imwrite(str(fullpath), frame)
-    print(str(x) + " downloaded")
+  # url = "http://video3.earthcam.com/fecnetwork/10072.flv/playlist.m3u8"
+  url = "http://video3.earthcam.com/fecnetwork/10147.flv/playlist.m3u8"
+  # url = "http://video3.earthcam.com/fecnetwork/10294.flv/playlist.m3u8"
+  # url = "http://video3.earthcam.com/fecnetwork/10347.flv/playlist.m3u8"
+  # url = "http://video3.earthcam.com/fecnetwork/10551.flv/playlist.m3u8"
 
+  cap = cv2.VideoCapture(url)
 
+  ret, frame = cap.read()
+  print(frame)
+  path = "/home/rschluet/imageDownloads"
+  filename = ("z_" + "1" + ".jpg")
+  fullpath = (str(path) + "/" + filename)
+  cv2.imwrite(str(fullpath), frame)
 
-  stream.release()
-  print("Stream closed")
-
-
+  #
+  #
+  #
+  # downloadCounter=0
+  #
+  # url="http://video3.earthcam.com/fecnetwork/10347.flv/playlist.m3u8"
+  # stream = cv2.VideoCapture(url)
+  # print("Stream opened")
+  # time.sleep(0.5)
+  # for x in range (0,10):
+  #   frame = stream.read()[1]
+  #   print(frame)
+  #   filename = ("z_" + str(x) + ".jpg")
+  #   fullpath = ("home/rschluet/imageDownloads/" + filename)
+  #   cv2.imwrite(str(fullpath), frame)
+  #   print(str(x) + " downloaded")
+  #
+  #
+  #
+  # stream.release()
+  # print("Stream closed")
+  #
+  #
