3f4197b969bc4f98e5795ca40667fe2eaaf76be2
rschlueter98 <rschlueter98@gmail.com>
2018-02-17 17:08:30 -0500

    fixed ()'s'

--- newDownloader.py
+++ newDownloader.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
   downloadCounter=0
 
   url="http://video3.earthcam.com/fecnetwork/10347.flv/playlist.m3u8"
-  cap = cv2.VideoCapture(url)
+  stream = cv2.VideoCapture(url)
   print("Stream opened")
   time.sleep(0.5)
   for x in range (0,10):
@@ -29,11 +29,11 @@ if __name__ == '__main__':
     filename = ("z_" + str(downloadCounter) + ".jpg")
     fullpath = ("home/rschluet/imageDownloads/" + filename)
     cv2.imwrite(str(fullpath), frame)
-    print(str(downloadCounter + " downloaded")
+    print(str(downloadCounter) + " downloaded")
 
 
 
-  cap.release()
+  stream.release()
   print("Stream closed")
 
 
