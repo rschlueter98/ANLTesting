58303836635ac6c26a734e95a97747a629ca66e5
Ryan Schlueter <rschlueter98@gmail.com>
2018-03-09 10:40:35 -0500

    switched from 6 second to 5 minutes per stream download for medium scale on argonne

--- twelveKdownloader.py
+++ twelveKdownloader.py
@@ -157,7 +157,7 @@ if __name__ == '__main__':
 
 
   # time to download (minutes), save or not
-  downloadImages(0.1, 1)
+  downloadImages(5, 1)
 
   # Wait while not done downloading yet
   while (len(cores_download_current) > 0):
