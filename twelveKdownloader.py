9f6a4e930dd750585425c691ce64d8a6a178fb01
Ryan Schlueter <rschlueter98@gmail.com>
2018-03-18 22:17:53 -0400

    30 minutes

--- twelveKdownloader.py
+++ twelveKdownloader.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
 
 
   # time to download (minutes), save or not
-  downloadImages(15, 1)
+  downloadImages(30, 1)
 
   # Wait while not done downloading yet
   while (len(cores_download_current) > 0):
